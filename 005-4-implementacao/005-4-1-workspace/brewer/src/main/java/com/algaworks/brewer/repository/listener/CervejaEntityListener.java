a71156ffd292d111ce0e95154a1a197f9ad4cc94
ukaliko <udineisilva@gmail.com>
2017-03-31 16:00:22 -0400

    #release-2.0.0 - migracao para Spring Boot 1.4.1

--- 005-4-implementacao/005-4-1-workspace/brewer/src/main/java/com/algaworks/brewer/repository/listener/CervejaEntityListener.java
+++ 005-4-implementacao/005-4-1-workspace/brewer/src/main/java/com/algaworks/brewer/repository/listener/CervejaEntityListener.java
@@ -3,25 +3,29 @@ package com.algaworks.brewer.repository.listener;
 import javax.persistence.PostLoad;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.storage.FotoStorage;
 
-/** Classe que fica escutando a entidade Cerveja, para quando toda vez que essa entidade for carregada do banco de dados
-    tambem recupere a url do thumbnail da foto, que sera utilizada ao pesquisar uma cerveja */
+/** Classe que fica escutando a entidade Cerveja, para quando toda vez que essa entidade for carregada
+ *  do banco de dados tambem recupere a url do thumbnail da foto, que sera utilizada ao pesquisar uma 
+ *  cerveja;
+ *  Essa classe nao e iniciado pelo Spring mas pelo JPA  */
+@Component
 public class CervejaEntityListener {
 
-	@Autowired
-	private FotoStorage fotoStorage;
+//	@Autowired
+//	private FotoStorage fotoStorage;
 	
 	// final no parametro do metodo nao permite implementar a expressao - cerveja = null;
 	@PostLoad
 	public void postLoad(final Cerveja cerveja){
 		// resolve no contexto corrente as injeção de dependencia, para todos os @Autowired da classe 
-		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+		//SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
 		
-		cerveja.setUrlFoto(fotoStorage.getUrl(cerveja.getFotoOrMock()));
-		cerveja.setUrlThumbnailFoto(fotoStorage.getUrl(FotoStorage.THUMBNAIL_PREFIX + cerveja.getFotoOuMock()));
+		cerveja.setUrlFoto(FotoStorage.URL + cerveja.getFotoOrMock());
+		cerveja.setUrlThumbnailFoto(FotoStorage.URL + FotoStorage.THUMBNAIL_PREFIX + cerveja.getFotoOuMock());
 	}
 }
